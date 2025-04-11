2fe0fcdd781eb4526612e224fc9f1bfcceaa7779
Saumil Dhumal <dhumalsaumil@gmail.com>
2025-04-11 13:48:00 +0530

    first commit

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,12 @@
+import EGO from "./EGO.png";
+import add from "./add.png";
+import list from "./list.png";
+import upload_area from "./photo.png";
+import parcel from "./parcel.png";
+export const assets = {
+  EGO,
+  add,
+  list,
+  upload_area,
+  parcel,
+};
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import Title from "../components/Title"
+
+const About = () => {
+  return (
+    <div className="">
+      <div className="text-2xl text-center pt-8 border-t ">
+        <Title   text1={"ABOUT"} text2={"US"} />
+      </div>
+    </div>
+  )
+}
+
+export default About
